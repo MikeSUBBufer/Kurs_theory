9d21874ad29894de39898aaf55f1bbd3b39e7394
Mikhail Subb <mike.zlyka@gmail.com>
2025-01-06 12:38:10 +0300

    first working commit

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-class Vector:
-    def _init_(self):
-        self.color = 0
-        self.angle = 0
 
-    def direction(self, v_x, v_y):
-        self.angle = math.atan(v_y/v_x)*180/math.pi
+# Создание сетки координат
+x, y = np.meshgrid(np.linspace(-100, 100, 1000), np.linspace(-100, 100, 1000))
 
-    '''def expect_colour(self):
-        self.color = v_x'''
-v_x=10
-v_y=10
-V1=Vector()
-V1.direction(v_x,v_y)
-print(V1.angle)
+# Время для построения линий тока
+t = 2
+
+# Поле скоростей в момент времени t
+v_x = -x * math.exp(t)
+v_y = y * math.exp(t)
+mag=np.sqrt(v_y**2+v_x**2)
+# Построение линий тока
+plt.figure(figsize=(8, 6))
+plt.streamplot(x, y, v_x, v_y, color=mag, density=3, linewidth=0.5)
+plt.title(f"Линии тока в момент времени t = {t:.2f}")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid()
+plt.show()
